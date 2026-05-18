11093d95d3e26de52d91f8386d26fb41945b4f21
Bruno <bruno9405@fito.br>
2026-05-18 15:29:25 -0300

    Adicionando teste de cadastro de produtos

--- src/test/product.test.js
+++ src/test/product.test.js
@@ -2,7 +2,16 @@ const request = require('supertest')
 const app = require('../app')
 const e = require('express')
 
-
+test('Deve cadastrar produto', async() => {
+    const response = await request(app)
+        .post('/routes/produtos')
+        .send({
+            nome: "Camiseta do Corinthians",
+            preco: 100.00
+        })
+    
+        expect(response.status).toBe(201)
+})
 
 test('Deve pegar produtos cadastrados', async() => {
     const response = await request(app)
