9bd63158da151a9d7376ac66de29fdab738e82d7
Bruno <bruno9405@fito.br>
2026-07-16 16:57:46 -0300

    Adicionando produtos já cadastrados

--- prisma/seed.js
+++ prisma/seed.js
@@ -13,9 +13,42 @@ async function main(){
             senha: senhaHash,
             role: 'Admin'
         }
+    })  
+
+    await prisma.produto.deleteMany()
+
+    await prisma.produto.createMany({
+        data: [
+            {
+                imagem: "1784213756234.jpeg",
+                nome: "Iphone 14 Pro Max",
+                descricao: "Iphone 14 Pro Max | 256GB de armazenamento | 16GB de RAM",
+                preco: 3500, 
+                estoque: 10,
+                categoria: "Eletrônicos" 
+            },
+
+            {
+                imagem: "1784213805511.jpg",
+                nome: "Mouse Gamer",
+                descricao: "RGB | 12000 DPI",
+                preco: 300,  
+                estoque: 30,
+                categoria: "Periféricos"
+            },
+
+            {
+                imagem: "1784229464366.png",
+                nome: "Camisa do Brasil",
+                descricao: "Camisa tradicional do Brasil",
+                preco: 500,  
+                estoque: 20,
+                categoria: "Roupa"
+            }
+        ]
     })
 
-    console.log('Admin criado')
+    console.log('Admain criado')
 }
 
 main()
